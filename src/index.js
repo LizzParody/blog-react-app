a30bde6f5a1be958ccb7d2849c43e0913ca247b8
lizparody <lizparody@gmail.com>
2017-10-13 15:52:54 -0500

    added the first index route

--- src/index.js
+++ src/index.js
@@ -3,27 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom'; //BrowserRouter interacts with the history library and decides what to do based on the change of the URL. Route is the react component tha we can render inside any other react component in the app (if the url looks like this show this components)
-
-import App from './components/app';
+//we deleted the App component because now that we have the router we don't have a central single component going on
 import reducers from './reducers';
+import PostIndex from './components/post_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends Component{
-  render(){ return <div>Hello</div>}
-}
-
-class Goodbye extends Component{
-  render(){ return <div>Goodbye</div>}
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Header {/* hardcode the header to all the pages */}
-        <Route path="/hello" component={Hello}/> {/* //path(string) and component are the two props we always pass on Route. If the user goes to this route, show this component*/}
-        <Route path="/goodbye" component={Goodbye}/>
+        <Route path="/" component={PostIndex} />
       </div>
     </BrowserRouter>
   </Provider>
