6d49d1002dcf0f12c2c30aea2ccbd73b9a9ae947
lizparody <lizparody@gmail.com>
2017-10-13 17:21:39 -0500

    added the posts list

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import FETCH_POSTS from '../actions/index';
+
+export default function (state = {}, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+       return _.mapKeys(action.payload.data, 'id');//[post1, post2] but we want and object {1: {post1}, 2: {post2}}, we use the lodash library for that
+
+    default:
+      return state;
+  }
+}
