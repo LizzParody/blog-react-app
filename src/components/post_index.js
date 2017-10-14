7ec1598b81dd3084d92f64a96722a2282303127e
lizparody <lizparody@gmail.com>
2017-10-14 08:55:08 -0500

    added the reducers into the component

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index'
 
 class PostIndex extends Component{
+  componentDidMount(){ //the component will render one time before we fetch the data. This function will be automatically called by react inmediatly after this component has shown up inside the DOM
+    this.props.fetchPosts();
+  }
   render () {
     return (
       <div>
@@ -10,4 +15,4 @@ class PostIndex extends Component{
   }
 }
 
-export default PostIndex;
+export default connect(null, { fetchPosts })(PostIndex); //null because we are not passing mapstatetoprops and a second argument instead of mapdispatchtoprops the action itself as an object
