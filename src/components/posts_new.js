08bed4d9857618529deaa7d9bf78fdbe34924239
lizparody <lizparody@gmail.com>
2017-10-16 16:21:26 -0500

    Added the validation of the form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -9,23 +9,25 @@ class PostsNew extends Component {
         <input
           className="form-control"
           type="text"
-          {...field.input}
+          {...field.input} // field.input is an object that has some properties and event handlers, the ... pass those props to input instead of doing onChange={this.input.onChange}
         />
+        {field.meta.error} {/* this property is automatically added to that field object from our validate function */}
       </div>
     )
   }
 
+  //to show the errors on the screen we can reference the field object that is passed to the render field function
   render() {
     return(
       <form>
         <Field
           label="Title"
-          name="title"
+          name="title" // the name here must be identical from the validate function
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -38,6 +40,27 @@ class PostsNew extends Component {
   }
 }
 
-export default reduxForm({ //we pass a single argument that is a function tha takes some #s of configurations options for it
-  form: 'PostsNewForm' //form: the name of the form, if there are multiple forms in a single page redux-form will handle the different forms correctly. PostsNewForm needs to be unique to not share it state with any other forms
+function validate(values) { // whenever the user tries to submit the form this function will be called, the argument is values by convention, values contains all the values that the user has enter in the form
+  // console.log(values) -> {title: "asdf", categories: "asdf", content: "asdf"}
+  const errors = {};
+
+  // validate the inputs from 'values'
+  if(!values.title){
+    errors.title = "Enter a title"; // if the user didn't enter a title, then we will add a property to the errors object of title and we're going to assigned to the msg it will be display to the user
+  }
+  if(!values.categories){
+    errors.categories = "Enter some categories";
+  }
+  if(!values.content){
+    errors.content = "Enter some content please";
+  }
+
+  // if errors is empty the form is valid to submit
+  // if errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({ // we pass a single argument that is a function tha takes some #s of configurations options for it
+  validate, // pass the configuration option called validate, the same as validate: validate
+  form: 'PostsNewForm' // form: the name of the form, if there are multiple forms in a single page redux-form will handle the different forms correctly. PostsNewForm needs to be unique to not share it state with any other forms
 })(PostsNew);
