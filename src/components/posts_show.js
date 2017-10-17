434441dcc672c6132287e93c6d5eaa132640e938
lizparody <lizparody@gmail.com>
2017-10-17 10:42:44 -0500

    finished the app

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params // provided by react router, match is the top level property, params is an object that list all the different wildcards tokens that exist inside the URL, get the id out the url
     this.props.fetchPost(id); // as soon as the component appears on the screen we're going to fetch that post
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    }); //action creator name deletePost, callback function, when we delete a post go back to the list of post
   }
 
   render() {
@@ -19,6 +26,12 @@ class PostsShow extends Component {
     return(
       <div>
         <Link to="/" className="btn btn-primary">Back to Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -31,4 +44,4 @@ function mapStateToProps({ posts }, ownProps) { // ownProps by convention, it's 
   return { post: posts[ownProps.match.params.id]} // instead of returning the list of post, we just return one single post that we care about
 }
 
-export default connect(mapStateToProps, { fetchPost } )(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost } )(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
-export default function (state = {}, action) { // the state object, the key is the individual post and the value are the actual post themselves, ideally we are going to get the post that gets returned from the API and inserted into our state object, we can't put the values from the actions creator because it doesn't have an ID asociated with it
+export default function (state = {}, action) { // the action is payload that contains the idea of post we just deleted, we need to reach our state object and get rid off the key and the value that match the particular id
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload); // look at the state object, if it has the key of post id, just drop it, omit it from that objet and return a new object that does not contain that id anymore
     case FETCH_POST:
       // const post = action.payload.data;
       // const newState = { ...state } //we already have some #s of post possibly that have been fetched  and stored inside the reducer (don't want to throw away the data that I fetch over time, we want to add to the existing app state level) ...state take all the existing post
